aa23749be53e5081da6e8177a0bc745c9711d807
FikruDemlew <fikrudemlew@gmail.com>
2025-06-21 23:37:45 +0300

    Deployment update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,8 +9,8 @@ const Navbar = ({ scrollTo }) => {
       <Link>
         <div className="flex sm:flex-row flex-col items-center mx-0 sm:mx-12 gap-2">
           <img
-            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D"
-            className="w-14 h-14 rounded-full"
+            src="/assets/profile2.jpg"
+            className="w-14 h-14 rounded-full "
             alt="Profile"
           />
           <h1 className="text-primary font-medium ml-3 text-3xl">Fikiru Demlew</h1>
@@ -45,10 +45,7 @@ const Navbar = ({ scrollTo }) => {
         <button onClick={scrollTo} className="text-white text-lg font-medium hover:!text-primary transition-colors duration-300 cursor-pointer">
           Projects
         </button>
-         <button onClick={scrollTo} className="text-white text-lg font-medium hover:!text-primary transition-colors duration-300 cursor-pointer">
-          Projects
-        </button>
-         <button onClick={scrollTo} className="text-white text-lg font-medium hover:!text-primary transition-colors duration-300 cursor-pointer">
+        <button onClick={scrollTo} className="text-white text-lg font-medium hover:!text-primary transition-colors duration-300 cursor-pointer">
             Contact
           </button>
       </div>
